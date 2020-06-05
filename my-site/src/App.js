d13099e12420ef5e49ee58f8e628d794de2939f6
Erin Davis <davisaerin@gmail.com>
2020-06-04 22:39:38 -0500

    have all pages showing (except skills which is temp removed) pushing to master so something shows on my actual site

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 import './App.css';
 import LandingPage from "./components/LandingPage.js";
 import SkillsPage from "./components/SkillsPage.js"
+import Projects from "./components/Projects.js";
+import About from "./components/About.js";
+import ContactForm from "./components/ContactForm.js";
 //import NavBar from "./components/NavBar.js";
 
 function App() {
@@ -15,16 +18,33 @@ function App() {
             <div className="nav-links">
             <Link to="/about" className="links">About Me</Link>
             <Link to="/home" className="links">Home</Link>
-            <Link to="/github" className="links">Github</Link>
-            <Link to="linkedin" className="links">LinkedIn</Link>
-            <Link to="skills" className="links">Skills</Link>
-            <Link to="projects" className="links">Projects</Link>
-            <Link to="contactme" className="links">Contact Me</Link>
+            <Link to="/github" className="links" >Github</Link>
+            <Link to="/linkedin" className="links">LinkedIn</Link>
+            <Link to="/skills" className="links">Skills</Link>
+            <Link to="/projects" className="links">Projects</Link>
+            <Link to="/contactme" className="links">Contact Me</Link>
             </div>
           </nav>
-          <Route exact path="/">
+          <LandingPage />
+          {/* Commenting out for the time being <SkillsPage />*/}
+          <Projects />
+          <About />
+          <ContactForm />
+
+        {/*  <Route exact path="/">
             <LandingPage />
           </Route>
+          <Route path="/skills">
+            <SkillsPage />
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>*/}
         </div>
       </main>
     </Router>
--- my-site/src/components/SkillsPage.js
+++ my-site/src/components/SkillsPage.js
@@ -3,8 +3,13 @@ import React from "react";
 export default function SkillsPage(){
 
   return (
-    <div>
+    <div className="skill-page">
       <h1>Skills</h1>
+      <ul>
+        <li>These</li>
+        <li>Are</li>
+        <li>Skills</li>
+      </ul>
     </div>
   );
 }
